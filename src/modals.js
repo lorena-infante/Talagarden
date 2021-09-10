73320cbe72ee7169201e2266fae8ce98ed3866bd
Lorena Infante <m.lorenainfanter@gmail.com>
2021-09-10 11:52:05 -0300

    adding a new modal for the first time

---
+++ src/modals.js
@@ -0,0 +1,15 @@
+const openModalButtons= document.querySelectorAll('[data-modal-target]');
+const closeModalButtons= document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById("overlay");
+
+/* openModalButtons.forEach(a =>{
+    a.addEventListener('click', ()=>{
+        const modal = document.querySelector('link-open-modal');
+        console.log(modal);
+    })
+}) */
+
+/* const openModal = () =>{
+    openModalButtons.addEventListener('click', console.log("click en link"));
+
+} */
