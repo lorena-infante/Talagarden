079730c52ff016ac9c0cfc5157cded239ee94489
Lorena Infante <m.lorenainfanter@gmail.com>
2021-09-24 10:56:13 -0300

    modifying pNames to undefined and getting json's
    getting only frutales of the page 1 on the catalog.js

--- src/catalog.js
+++ src/catalog.js
@@ -11,70 +11,51 @@
                 nombre_producto,
                 precio,
                 medidas,
-                foto;
-        //naming DOM variables fot the "P" elements on frutales-box
-        /* document.querySelectorAll('.text-container-frutal>p') */
-        
-        let pName = document.querySelectorAll('.text-container-frutal>p:nth-child(1)'),//pilas aqwui era antes getElementById("pName") 
-        pPrice = document.getElementById("pPrice"),
-        pHeight = document.getElementById("pHeight");
-        console.log(pName);  
-        /* console.log(pPrice);  
-        console.log(pHeight);  */   
+                foto; 
 
         //recorriendo la data y chequeando que solo traiga los árboles de la categoría "FRU"
-        //counter of pName
+        //solo trae los frutales
+        const frutales = catalogData.filter(producto => producto.id_categoria === "FRU");
+        console.log(frutales);
+        //el length de los futales
+        const frutalesLength=frutales.length; 
+        console.log(frutalesLength);
+        //counter of pName (to iterate through the forEach. Includes the new name on the p:nth-child(1) )
         let i=0;
-            catalogData.forEach((producto,index) => {
-                let pName = document.querySelectorAll('.text-container-frutal>p:nth-child(1)'),//pilas aqwui era antes getElementById("pName") 
-                pPrice = document.getElementById("pPrice"),
-                pHeight = document.getElementById("pHeight");
-                 
-                if(producto.id_categoria === "FRU"){  
-                    console.log(producto); 
-                    //el length de los futales
-                    const frutales = catalogData.filter(producto => producto.id_categoria === "FRU");
-                    const frutalesLength=frutales.length; 
-                    console.log(frutalesLength);
-                    /* let prodIndexIf= index;      
-                    console.log(prodIndexIf); */
-                    nombre_producto = producto.nombre_producto;
-                    precio = producto.precio;
-                    medidas = producto.medidas;
-                    foto = producto.foto;
-                    
-                    pName[i].textContent= nombre_producto;
-                    i++;
-                    //for(i; i<frutalesLength; i++){
-                        //pName.item(i).textContent=nombre_producto;
+        //get number of box-frutales per page (to iterate until that amount of divs)
+        let boxFrutales=document.querySelectorAll('.box-frutales');
+        let boxFrutalesLength=boxFrutales.length
+        console.log(boxFrutalesLength);
+            frutales.forEach(producto => {
+                //si el numero de cajas en los divs es igual al numero de iteraciones que ha realizado (i), entonces debe detenerse el recorrido que ubica en el pName los nombres de los árboles
+                if (i === boxFrutalesLength) return false;
 
-                        //pName.item(i).textContent= nombre_producto;
+                //naming DOM variables fot the "P" elements on frutales-box
+                let pName = document.querySelectorAll('.text-container-frutal>p:nth-child(1)'),
+                pPrice = document.querySelectorAll('.text-container-frutal>p:nth-child(2)'),
+                pHeight = document.querySelectorAll('.text-container-frutal>p:nth-child(3)');
+ 
+                console.log(producto); 
+                //console.log(pPrice); 
+                //console.log(pHeight);
 
-                    //} 
-                    /* pName.textContent = nombre_producto;
-                    console.log(pName.textContent); */
-                    
-                    /* pName.item(0).textContent= nombre_producto
-                    console.log(pName.textContent); */
-                    //console.log(nombre_producto);
-                    //console.log(pName.item(index)); 
-                    console.log(nombre_producto,precio,medidas,foto); 
-                    //pName.textContent = nombre_producto;
-                    
-                }
+                nombre_producto = producto.nombre_producto;
+                precio = producto.precio;
+                medidas = producto.medidas;
+                foto = producto.foto;
                 
+                //places the right name on the right place
+                pName[i].textContent= `${nombre_producto}`;
+                //iterates to keep on going through the page
+                i++;
                 
+                console.log(nombre_producto,precio,medidas,foto); 
+
                 //pName.textContent= `${nombre_producto}`;
                 /*pPrice.textContent= `$${precio}`;
                 pHeight.textContent= `Altura: ${medidas}`; */
             });
-            /* pName.forEach((name,index)=>{
-                name.textContent = nombre_producto;
-                let prodIndex= index;  
-                console.log(prodIndex);
-                pName.item(prodIndex).textContent= nombre_producto;
-                 
-            });  */
+            
         
         
 
