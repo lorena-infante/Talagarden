2be1f2017c55acb2289cad9b7dc322d571a2a1c8
Lorena Infante <m.lorenainfanter@gmail.com>
2021-10-08 19:55:43 -0300

    ya trae al modal los datos del json, menos la luz, y falta arreglar el primer elemento que no trae imagen
    -revisar qué pasa con la compatibilidad del filter en edge y chrome

--- src/modals_frutales.js
+++ src/modals_frutales.js
@@ -44,15 +44,72 @@ function openModal(modal,prodName){
     if(modal === null) return
     modal.classList.add('active');
     overlay.classList.add('active');
-    //Llamando elementos del DOM
-    //modal.modal-body.modal-title (esa es la ruta que llamo aqui)
-    const mTitle = modal.children[1].childNodes[1];
-    //console.log(mTitle);
-    //Assigning dynamic title to modal
-    mTitle.textContent = `${prodName}`; 
+    //trae el objeto JSON desde el catalog.json
+    getJSONObj(modal,prodName);
     
 }
- 
+
+//trae los objetos JSON en base al nombre del producto
+const getJSONObj = async(modal,prodName)=>{
+    try{
+        const resObject = await axios('./src/catalog.json'); 
+        const catalogData= resObject.data.data;
+        //console.log(catalogData)
+          //recorriendo la data y chequeando que solo traiga los árboles con el nombre de producto asignado (que trae del HTML)
+        const object = catalogData.filter(producto => producto.nombre_producto === `${prodName}`);
+        console.log(object);
+        //object es un array con un único objeto dentro, por eso siempre va en posición 0
+        let id_categoria = object[0].id_categoria;
+        let categoria = object[0].categoria;
+        let id_producto = object[0].id_producto;
+        let precio = object[0].precio;
+        let medidas = object[0].medidas;
+        let nombre_producto = object[0].nombre_producto;
+        let foto = object[0].foto;
+        let descripcion = object[0].descripcion;
+        let cuidados = object[0].cuidados;
+        let luz = object[0].luz;
+        //console.log(luz);
+        //ubica en el modal cada uno de los datos traidos por el JSON
+        setModalContent(modal,nombre_producto,foto,descripcion,cuidados,luz);
+
+
+        }catch(error){
+        console.log(`el error es: ${error}`);
+    }
+
+}
+
+//ubica en el modal cada uno de los datos traidos por el JSON
+function setModalContent(modal,nombre_producto,foto,descripcion,cuidados,luz){
+    //Llamando elementos del DOM
+    //#modal > div:nth-child(2) > div:nth-child(1)
+    const mTitle = modal.children[1].childNodes[1];
+    //#modal > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > img:nth-child(1)
+    const mSliderCont = modal.children[1].children[1].children[0].children[1].children[0].children[0]
+    /* solo funciona para el almendro modal.children[1].children[1].children[0].children[1].children[0].children[0].children[0]; */
+    /*#modal > div:nth-child(2) > div:nth-child(4) > p:nth-child(2) */
+    const mDescrip = modal.children[1].children[3].children[1];
+    /*#modal > div:nth-child(2) > div:nth-child(4) > p:nth-child(4) */
+    const mCares = modal.children[1].children[3].children[3];
+    /*#modal > div:nth-child(2) > div:nth-child(4) > div:nth-child(6)*/
+    const mLight = modal.children[1].children[3].children[5];
+
+    console.log(mLight);
+
+
+    //let newImg= new Image();
+    //newImg.src = `${foto}`;
+    console.log(mSliderCont)
+    //console.log(mTitle);
+    //Assigning dynamic titles & content to modal
+    mTitle.textContent = `${nombre_producto}`; 
+    console.log(foto[0]);
+    mSliderCont.src = `${foto}`; 
+    mDescrip.textContent = `${descripcion}`;
+    mCares.textContent = `${cuidados}`;
+
+}
 function closeModal(modal){
     
     if(modal === null) return
